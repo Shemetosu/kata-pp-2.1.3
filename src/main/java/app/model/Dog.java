142baf6aa98ed077096a0780b2ee5f7eca19ae00
Vladimir Shemetenko <ov.shtk@gmail.com>
2025-03-10 17:23:52 +0300

    Выполнил задание. Тесты прошли успешно

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -2,7 +2,11 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("dog")
 public class Dog extends Animal{
 
+    @Override
+    public String toString() {
+        return "Im a Dog";
+    }
 }
